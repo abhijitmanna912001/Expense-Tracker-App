7b1dadb1098b412b59898603292ca87f3ee6eefe
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-12-16 00:24:21 +0530

    File Upload Cloudinary Setup Completed

---
+++ constants/index.ts
@@ -0,0 +1,5 @@
+export const CLOUDINARY_CLOUD_NAME = process.env
+  .EXPO_PUBLIC_CLOUDINARY_CLOUD_NAME as string;
+
+export const CLOUDINARY_UPLOAD_PRESET = process.env
+  .EXPO_PUBLIC_CLOUDINARY_UPLOAD_PRESET as string;
--- services/userService.ts
+++ services/userService.ts
@@ -1,12 +1,28 @@
 import { firestore } from "@/config/firebase";
 import { ResponseType, UserDataType } from "@/types";
 import { doc, updateDoc } from "firebase/firestore";
+import { uploadFileCloudinary } from "./ImageService";
 
 export const updateUser = async (
   uid: string,
   updatedData: UserDataType
 ): Promise<ResponseType> => {
   try {
+    if (updatedData.image?.uri) {
+      const imageUploadRes = await uploadFileCloudinary(
+        updatedData.image,
+        "users"
+      );
+
+      if (!imageUploadRes.success)
+        return {
+          success: false,
+          msg: imageUploadRes.msg || "Image upload failed",
+        };
+
+      updatedData.image = imageUploadRes.data;
+    }
+    
     const userRef = doc(firestore, "users", uid);
     await updateDoc(userRef, updatedData);
     return { success: true, msg: "User updated successfully" };
